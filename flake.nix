cd0bd83710fcc806a9da79ffd1d6b311cd1b0ca3
hugosenari <hugosenari@gmail.com>
2022-03-12 00:11:14 -0300

    chore: re index nimscripter

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Easy to use Nim/Nimscript interop, for scripting logic in compiled binaries.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimscripter-master".url = "path:./master";
+  inputs."nimscripter-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimscripter-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
